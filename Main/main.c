44a9ec5f927e0d009d3633a7bc5667bf9a36d7cb
root <root@debian>
2022-05-24 14:51:14 +0200

    add folder Main and TF

---
+++ Main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "window.h"
+#include "input.h"
+#include "draw.h"
+#include "init.h"
+
+int main() {
+    Win *app = malloc(sizeof(Win));
+
+    if (initSDL(app) < 0) {
+        return -1;
+    }
+
+    while (input_handler() == 0) {
+        prepareCanvas(app);
+        presentCanvas(app);
+        SDL_Delay(16);
+    }
+    
+    return 0;
+}
