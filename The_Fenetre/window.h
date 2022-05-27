be4ce3061ee02d0e779b3aaadc8c3140fed28d37
root <root@debian>
2022-05-27 16:42:05 +0200

    la N

--- The_Fenetre/window.h
+++ The_Fenetre/window.h
@@ -3,8 +3,8 @@
 
 #include <SDL2/SDL.h>
 
-#define WINDOW_WIDTH 480
-#define WINDOW_HEIGHT 360
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 600
 #define WINDOW_NAME "Star_Renard"
 
 typedef struct
