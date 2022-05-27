57719fec39f117a45e91f629931ffe2f4052e016
root <root@debian>
2022-05-27 10:29:38 +0200

    [EDIT] modification file for debug movement

--- Main/input.c
+++ Main/input.c
@@ -16,32 +16,35 @@ int input_handler(Entity *entity) {
                 case SDLK_DOWN:
                     entity->pos_y += 4;
                     break;
-            }
-            break;
-            case SDLK_LEFT :
-            printf("key is left\n");
-            switch (event.key.keysym.sym) {
-                case SDLK_LEFT:
-                    entity->pos_x += 4;
-                    break;
-            }
-            break;
-        case SDL_KEYUP :
-            printf("key is up\n");
-            switch (event.key.keysym.sym) {
                 case SDLK_UP:
                     entity->pos_y -= 4;
                     break;
-            }
-            break;
-            case SDLK_RIGHT :
-            printf("key is right\n");
-            switch (event.key.keysym.sym) {
                 case SDLK_RIGHT:
+                    entity->pos_x += 4;
+                    break;
+                case SDLK_LEFT:
                     entity->pos_x -= 4;
                     break;
             }
             break;
+            }
+        //     break;
+        // case SDL_KEYUP :
+        //     printf("key is up\n");
+        //     switch (event.key.keysym.sym) {
+        //         case SDLK_UP:
+        //             entity->pos_y -= 4;
+        //             break;
+        //     }
+        //     break;
+        //     case SDLK_RIGHT :
+        //     printf("key is right\n");
+        //     switch (event.key.keysym.sym) {
+        //         case SDLK_RIGHT:
+        //             entity->pos_x -= 4;
+        //             break;
+        //     }
+        //     break;
             // case SDLK_LEFT :
             // printf("key is left\n");
             // switch (event.key.keysym.sym) {
