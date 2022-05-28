726f318d06452767146aacbd5ba9deae0b4eb4ab
root <root@debian>
2022-05-28 14:37:02 +0200

    [edit] add enemy

--- Main/draw.c
+++ Main/draw.c
@@ -1,30 +1,35 @@
 #include "draw.h"
 
-void prepareCanvas(Win *app) {
+void prepareCanvas(Win *app)
+{
 
     SDL_SetRenderDrawColor(app->renderer, 96, 128, 255, 255);
     SDL_RenderClear(app->renderer);
 }
 
-void presentCanvas(Win *app) {
+void presentCanvas(Win *app)
+{
     SDL_RenderPresent(app->renderer);
 }
 
-SDL_Texture *loadTexture(Win *app, char *img_path) {
+SDL_Texture *loadTexture(Win *app, char *img_path)
+{
     SDL_Texture *texture;
 
     SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading: %s", img_path);
     texture = IMG_LoadTexture(app->renderer, img_path);
-    if (texture == NULL) {
+    if (texture == NULL)
+    {
         printf("Failed to load texture %s\n", IMG_GetError());
     }
     return texture;
 }
 
-void drawEntity(Win *app, Entity *entity) {
-    SDL_Rect dest= {entity->pos_x, entity->pos_y, entity->width, entity->height};
-
+void drawEntity(Win *app, Entity *entity, Enemy *enemy)
+{
+    SDL_Rect dest = {entity->pos_x, entity->pos_y, entity->width, entity->height};
     SDL_RenderCopy(app->renderer, entity->texture, NULL, &dest);
-    
-}
 
+    SDL_Rect Pos = {enemy->pos_x, enemy->pos_y, enemy->width, enemy->height};
+    SDL_RenderCopy(app->renderer, enemy->texture, NULL, &Pos);
+}
--- Main/main.c
+++ Main/main.c
@@ -8,6 +8,7 @@ int main()
 {
     Win *app = malloc(sizeof(Win));
     Entity *vaisseau = malloc(sizeof(Entity));
+    Enemy *enemy = malloc(sizeof(Enemy));
 
     if (initSDL(app) < 0)
     {
@@ -20,37 +21,16 @@ int main()
     vaisseau->height = 50;
     vaisseau->texture = loadTexture(app, "Ressource/vaisseau.png");
 
-    while (input_handler(vaisseau) == 0)
-    {
-        prepareCanvas(app);
-        drawEntity(app, vaisseau);
-        presentCanvas(app);
-        SDL_Delay(16);
-    }
-
-    return 0;
-}
-
-int enemy()
-{
-    Win *app = malloc(sizeof(Win));
-    Entity *enemy = malloc(sizeof(Entity));
-
-    if (initSDL(app) < 0)
-    {
-        return -1;
-    }
-
     enemy->pos_x = 100;
     enemy->pos_y = 100;
     enemy->width = 50;
     enemy->height = 50;
     enemy->texture = loadTexture(app, "Ressource/enemy.png");
 
-    while (input_handler(enemy) == 0)
+    while (input_handler(vaisseau) == 0)
     {
         prepareCanvas(app);
-        drawEntity(app, enemy);
+        drawEntity(app, vaisseau, enemy);
         presentCanvas(app);
         SDL_Delay(16);
     }
